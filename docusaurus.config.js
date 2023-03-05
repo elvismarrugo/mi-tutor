be4d987bed0bb5e1d1a13b3e5bfef4b5b1e4593c
MrElvix <elvismarrugo@gmail.com>
2023-03-05 17:01:13 -0500

    build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://elvismarrugo.github.io/mi-tutor',
+  url: 'https://elvismarrugo.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
